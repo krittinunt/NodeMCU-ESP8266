f9714dae43d5e133bbe4e800fbdca32b3a49cb9a
krittinunt <krittinunt@gmail.com>
2017-10-14 22:28:03 +0700

    Add files via upload

---
+++ [1] WiFi_Connect_StaticIP/WiFi_Connect_StaticIP/WiFi_Connect_StaticIP.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "Elec_Surat";
+const char* password = "91992189327";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  IPAddress local_ip = {192, 168, 1, 50};
+  IPAddress gateway = {192, 168, 1, 1};
+  IPAddress subnet = {255, 255, 255, 0};
+  WiFi.config(local_ip, gateway, subnet);
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
