af41cf80ae81732e19d2a22dec4fe174e6a84de3
krittinunt <krittinunt@gmail.com>
2017-10-14 22:29:08 +0700

    Add files via upload

---
+++ [3] TCP_Client/TCP_Client/TCP_Client.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+
+#define SERVER_PORT       8000
+IPAddress server_ip = {192, 168, 1, 13};
+
+const char* ssid = "Elec_Surat";
+const char* password = "91992189327";
+
+WiFiServer server(SERVER_PORT);
+WiFiClient client;
+
+uint16_t i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Connect TCP Server");
+  while (!client.connect(server_ip, SERVER_PORT))
+  {
+    Serial.println(".");
+    delay(100);
+  }
+  Serial.println("Success");
+  ESP.wdtDisable();
+  ESP.wdtEnable(WDTO_8S);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ESP.wdtFeed();
+  while (client.available())
+  {
+    uint8_t data = client.read();
+    Serial.write(data);
+  }
+  client.println("KRITTINUNT-NODE");
+  client.println("I Love gutur.");
+  delay(1000);
+}
---
+++ [5] Hello_WebServer/HelloWebServer/HelloWebServer.ino
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "Elec_Surat";
+const char* password = "91992189327";
+WiFiServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+  Serial.println(""); Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
+  ESP.wdtDisable();
+  ESP.wdtEnable(WDTO_8S);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ESP.wdtFeed();
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+
+  Serial.println("new client");
+  while (!client.available())
+  {
+    delay(1);
+  }
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  String web = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  web += "<html>\r\n";
+  web += "<body>\r\n";
+  web += "<h1>Hello web server</h1>\r\n";
+  web += "<br>Name:Krittinunt Chobtrong\r\n>";
+  web += "<br>Tel:0505512022\r\n>";
+  web += "<br><img src=\"https://pbs.twimg.com/profile_images/593465700682956800/Y_I3_pnk.jpg\">\r\n";
+  web += "</body>\r\n";
+  web += "</html>\r\n";
+  client.print(web);
+}
